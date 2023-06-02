5c972c776b69e0a739ef9c0738223f4ac59b434e
kino-ma <ma@kino.ma>
2023-06-02 22:39:23 +0900

    feat: add mani function

--- app/serv.py
+++ app/serv.py
@@ -71,6 +71,11 @@ def index():
     return render_template('index.html', code=code, error=error)
 
 
+def main():
+    app.run(
+        host="0.0.0.0",
+        port=5000)
+    
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
