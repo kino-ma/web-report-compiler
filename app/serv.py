cd493493d2b8eefbccb30a03e80a87c4be6f3398
kino-ma <ma@kino.ma>
2023-12-13 15:09:51 +0900

    fix: various errors

--- app/serv.py
+++ app/serv.py
@@ -1,58 +1,57 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, render_template, send_file
 import os
 import sys
-import hashlib
 import subprocess
+
+from flask import Flask, request, render_template, send_file
 
 TIMEOUT = 300
 PDF_MIMETYPE = "application/pdf"
 
 app = Flask(__name__)
-cmd = 'pandoc -F pandoc-crossref ./data/resume.md -o ./data/resume.pdf --citeproc --bibliography=/root/src/resume.bib --pdf-engine lualatex -V luatexjapresetoptions=ipa -N'.split(
-#cmd = 'pandoc -F pandoc-crossref /app/resume.md -o /app/resume.pdf --bibliography=/root/src/resume.bib --pdf-engine lualatex -V luatexjapresetoptions=ipa -N'.split(
-    ' ')
+cmd = "pandoc -F pandoc-crossref ./data/resume.md -o ./data/resume.pdf --citeproc --bibliography=/root/src/resume.bib --pdf-engine lualatex -V luatexjapresetoptions=ipa -N".split(
+    # cmd = 'pandoc -F pandoc-crossref /app/resume.md -o /app/resume.pdf --bibliography=/root/src/resume.bib --pdf-engine lualatex -V luatexjapresetoptions=ipa -N'.split(
+    " "
+)
 
-token = os.environ.get('TOKEN')
+token = os.environ.get("TOKEN")
 
 
 def exec():
     # run code
-    error = ''
+    error = ""
     success = True
     try:
         process = subprocess.run(
-            cmd,
-            capture_output=True,
-            encoding='utf-8',
-            timeout=TIMEOUT)
+            cmd, capture_output=True, encoding="utf-8", timeout=TIMEOUT
+        )
 
-        success = success and  process.returncode == 0
+        success = success and process.returncode == 0
         error = process.stderr
-    except subprocess.TimeoutExpired as e:
+    except subprocess.TimeoutExpired:
         success = False
-        error = 'Timed out'
+        error = "Timed out"
 
     return success, error
 
 
-@app.route(f"/{token}", methods=['GET', 'POST'])
+@app.route(f"/{token}", methods=["GET", "POST"])
 def index():
-    code = ''
-    error = ''
+    code = ""
+    error = ""
 
-    if request.method == 'POST':
+    if request.method == "POST":
         code = request.form["code"]
 
         # if request has body, use it as MD input
         if not code:
             # Save MD to file
-            filename = './data/src.md'
-            with open(filename, mode='r') as f:
+            filename = "./data/src.md"
+            with open(filename, mode="r") as f:
                 code = f.read()
 
-        with open('./data/resume.md', mode='w') as f:
+        with open("./data/resume.md", mode="w") as f:
             f.write(code)
 
         # or, read original content
@@ -61,24 +60,24 @@ def index():
         filename = "resume.pdf"
 
         if success:
-            response = send_file(filename, as_attachment=True,
-                                 attachment_filename=filename,
-                                 mimetype=PDF_MIMETYPE)
-            response.headers['Content-Disposition'] = 'inline; filename=%s' % filename
-            print('stderr:', error, file=sys.stderr)
+            response = send_file(
+                filename,
+                as_attachment=True,
+                attachment_filename=filename,
+                mimetype=PDF_MIMETYPE,
+            )
+            response.headers["Content-Disposition"] = "inline; filename=%s" % filename
+            print("stderr:", error, file=sys.stderr)
             return response
 
-        print('stderr:', error, file=sys.stderr)
+        print("stderr:", error, file=sys.stderr)
 
-    return render_template('index.html', code=code, error=error)
+    return render_template("index.html", code=code, error=error)
 
 
 def main():
-    app.run(
-        host="0.0.0.0",
-        port=5000)
-    
+    app.run(host="0.0.0.0", port=5000)
+
+
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000)
+    app.run(host="0.0.0.0", port=5000)
